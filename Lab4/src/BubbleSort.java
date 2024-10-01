2805aa7fa70efe9e322c21e5037d59c9fe05db5c
Jwahn1 <javierwith@gmail.com>
2024-10-01 15:32:57 -0300

    Lab 4 Started

---
+++ Lab4/src/BubbleSort.java
@@ -0,0 +1,32 @@
+
+
+/*
+Bubble Sort
+This class tests the code for Lab4: Exercise1. It calls the sort method to
+sort an array of size n and prints information about running time.
+*/
+import java.util.*;
+public class BubbleSort{
+    public static void main(String[] args){
+        //TODO
+        //prompt the user to enter the value of n
+        //create an integer array of size n with random integers
+        //the range of random integers is from 1 to n
+
+        long startTime, endTime, executionTime;
+        startTime = System.currentTimeMillis();
+
+        //call to the sort method
+
+        endTime = System.currentTimeMillis();
+        executionTime = endTime - startTime;
+
+        //display the executionTIme
+    }
+    public static int[] sort(int[] arr){
+        //TODO
+        //sort and return the integer arr of size n
+        int[] dummy = {1,2};
+        return dummy;
+    }
+}
