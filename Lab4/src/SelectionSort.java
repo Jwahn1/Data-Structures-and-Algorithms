d11822a94b927d3e12073d7b1009cd0f5f8f1ffc
Jwahn1 <javierwith@gmail.com>
2024-10-01 16:48:36 -0300

    sort() needs implementing but everything else is implemented, borrowed code from BubbleSort.java

--- Lab4/src/SelectionSort.java
+++ Lab4/src/SelectionSort.java
@@ -6,20 +6,47 @@ sort an array of size n and prints information about running time.
 import java.util.*;
 public class SelectionSort{
     public static void main(String[] args){
-        //TODO
-        //prompt the user to enter the value of n
-        //create an integer array of size n with random integers
+
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("Please enter the size of the array");
+        int arraySize = in.nextInt();
+
+        int[] unsortedArray = new int[arraySize];
+        int[] sortedArray;
+
+        //create array here and fill it with random numbers
         //the range of random integers is from 1 to n
+        for (int i = 0; i < arraySize  ; i++) {
+            unsortedArray[i] = rand.nextInt(100);
+            System.out.print(unsortedArray[i]+" ");
+        }
+        System.out.println("");
+
+
         long startTime, endTime, executionTime;
         startTime = System.currentTimeMillis();
+
         //call to the sort method
+        sortedArray = sort(unsortedArray);
+
+        for (int i = 0; i < arraySize  ; i++) {
+            System.out.print(sortedArray[i]+" ");
+        }
+
         endTime = System.currentTimeMillis();
         executionTime = endTime - startTime;
+
+        System.out.println("\ntime to run " + executionTime + "ms");
+
         //display the executionTIme
     }
     public static int[] sort(int[] arr){
         //TODO
         //sort and return the integer arr of size n
+
+        return arr;
     }
     //method swaps the value in [arrayPos] with the value in [ArrayPos+1] and places the value in [arrayPos] in [arrayPos+1]
     public static int[] swap(int[] array, int smallerNum, int largerNum, int arrayPos){
